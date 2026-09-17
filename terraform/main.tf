1c333fea8208a9a3342195f9c7e5ac2a42c4025c
Bothub-Harsh <harshmeshram838@gmail.com>
2026-09-17 09:32:22 +0530

    added

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "aws-infrastructure-vpc"
+  }
+}
+
+resource "aws_subnet" "public_az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "public-az1"
+  }
+}
+
+resource "aws_subnet" "public_az2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "public-az2"
+  }
+}
